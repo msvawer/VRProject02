fad71ed5a58aeb56bcb219541866e59b041159a2
MichVawer <michelle.vawer@icloud.com>
2022-03-28 16:46:57 -0400

    created singleton in objectPooler

--- Assets/ObjectPooler.cs
+++ Assets/ObjectPooler.cs
@@ -13,6 +13,18 @@ public class ObjectPooler : MonoBehaviour
 
     }
 
+    public static ObjectPooler Instance;
+
+    #region Singleton
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    #endregion
+
+
+
     public List<Pool> pools;
     public Dictionary<string, Queue<GameObject>> poolDictionary;
 
@@ -22,6 +34,42 @@ public class ObjectPooler : MonoBehaviour
     {
         poolDictionary = new Dictionary<string, Queue<GameObject>>();
 
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+
+        }
+
+    }
+
+
+    public GameObject SpawnFromPool (string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey (tag))
+        {
+            Debug.LogWarning("Pool with tag" + tag + " doesn't exist");
+            return null;
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
     }
 
     
