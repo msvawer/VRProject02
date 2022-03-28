2c076d17ada34efc5ce6c0772e3775c315e74e68
MichVawer <michelle.vawer@icloud.com>
2022-03-28 14:50:05 -0400

    Started ObjectPooling script

---
+++ Assets/ObjectPooler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+    }
+
+    
+}
